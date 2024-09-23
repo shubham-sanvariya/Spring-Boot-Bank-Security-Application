8777bfd0cba031f67f3a1f6c32b17f0fde7a607b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-23 17:29:28 +0530

    adding Account Entity

--- src/main/java/com/security/bank/entity/Account.java
+++ src/main/java/com/security/bank/entity/Account.java
@@ -1,5 +1,7 @@
 package com.security.bank.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,24 +22,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Entity
-public class Investment {
+public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private InvestmentType investmentType;
+    private AccountType accountType;
 
-    private String risk;
+    private String status;
 
-    private double amount;
+    private double balance;
 
-    private float returns;
+    private float interestRate;
 
-    private String duration;
+    @Enumerated(EnumType.STRING)
+    private BranchType branch;
 
-    private String companyName;
+    private String proof;
+
+    private Date openingDate;
+
+    private Long accountNumber;
+
+    @OneToOne
+    private Nominee nominee;
+
+    @OneToOne
+    private Card card;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
