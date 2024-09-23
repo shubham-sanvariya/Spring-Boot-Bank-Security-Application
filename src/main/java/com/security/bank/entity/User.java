e6736395d8093e1c8c07c65171a2de65991e9e4b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-23 17:01:32 +0530

    adding relational mapping bet^n user and role

--- src/main/java/com/security/bank/entity/User.java
+++ src/main/java/com/security/bank/entity/User.java
@@ -1,9 +1,12 @@
 package com.security.bank.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,4 +35,8 @@ public class User {
     private Long number;
 
     private String identityProof;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
