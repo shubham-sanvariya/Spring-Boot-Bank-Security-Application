024151104508c2bdcc0ab78082d395f60c114003
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-24 10:47:59 +0530

    adding CustomUserDetail Service

--- src/main/java/com/security/bank/repository/UserRepository.java
+++ src/main/java/com/security/bank/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.security.bank.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import com.security.bank.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/security/bank/security/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.security.bank.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.security.bank.repository.UserRepository;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+    
+}
