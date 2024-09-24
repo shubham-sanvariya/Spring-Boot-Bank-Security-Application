bfe36285b867cfd69452f227829913baf1389ea0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-24 11:33:50 +0530

    adding getClaimsFromToken method in JwtAuthenticationHelper

--- src/main/java/com/security/bank/jwt/JwtAuthenticationHelper.java
+++ src/main/java/com/security/bank/jwt/JwtAuthenticationHelper.java
@@ -2,7 +2,18 @@ package com.security.bank.jwt;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Component
 public class JwtAuthenticationHelper {
+
+	private String secret = "thisisacodingninjasdemonstrationforsecretkeyinspringsecurityjsonwebtokenauthentication";
     
+    public Claims getClaimsFromToken(String token){
+        return Jwts.parserBuilder().setSigningKey(secret.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+    }
 }
