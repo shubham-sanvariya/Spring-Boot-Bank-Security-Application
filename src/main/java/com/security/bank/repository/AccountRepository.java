27f61b9351735091f6e3cc7966872258eda48ea5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-24 18:00:59 +0530

    adding findAllActiveAccounts method in account repo

--- src/main/java/com/security/bank/repository/AccountRepository.java
+++ src/main/java/com/security/bank/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.security.bank.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.security.bank.entity.Account;
@@ -10,4 +12,8 @@ import com.security.bank.entity.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long>{
     Optional<Account> findByAccountNumber(Long accountNumber);
+
+    @Query("SELECT ac FROM Account ac WHERE ac.status = 'ACTIVE' ")
+    List<Account> findAllActiveAccounts();
+
 }
