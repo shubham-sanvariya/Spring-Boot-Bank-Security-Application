c607df41cd17e339388d461017a6ec0774fe772f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-24 11:03:36 +0530

    adding Authentication Manger Bean in Security Config

--- src/main/java/com/security/bank/config/BankSecurityConfig.java
+++ src/main/java/com/security/bank/config/BankSecurityConfig.java
@@ -2,6 +2,8 @@ package com.security.bank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class BankSecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+        return builder.getAuthenticationManager();
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder(){
