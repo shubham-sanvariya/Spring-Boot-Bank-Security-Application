2c25cde6d44b8d73fddcc04f3d27647eeba5e300
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-24 15:51:45 +0530

    adding generate Token method in jwt helper class

--- src/main/java/com/security/bank/jwt/JwtAuthenticationHelper.java
+++ src/main/java/com/security/bank/jwt/JwtAuthenticationHelper.java
@@ -1,22 +1,40 @@
 package com.security.bank.jwt;
 
 import java.util.Date;
+import java.util.HashMap;
+
+import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.stereotype.Component;
 
+import com.security.bank.dto.JwtRequest;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtAuthenticationHelper {
 
 	private String secret = "thisisacodingninjasdemonstrationforsecretkeyinspringsecurityjsonwebtokenauthentication";
+	private static final long JWT_TOKEN_VALIDITY = 60 * 60;
     
     public Claims getClaimsFromToken(String token){
         return Jwts.parserBuilder().setSigningKey(secret.getBytes())
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
+    }
+
+    public String generateToken(JwtRequest jwtRequest){
+        return Jwts.builder()
+                .setClaims(new HashMap<String ,Object>())
+                .setSubject(jwtRequest.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS512.getJcaName()),SignatureAlgorithm.HS512)
+                .compact();
+
     }
 
     public String getUsernameFromToken(String token){
